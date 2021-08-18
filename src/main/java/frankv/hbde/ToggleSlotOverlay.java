8099eed35617e3cc686340bd4f92d70cbb3893c6
frank89722 <frank89722@gmail.com>
2021-08-18 18:38:18 +0800

    Fixed Overlay render at wrong position when window scale changed Fixed Texture color

--- src/main/java/frankv/hbde/ToggleSlotOverlay.java
+++ src/main/java/frankv/hbde/ToggleSlotOverlay.java
@@ -4,22 +4,23 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 public class ToggleSlotOverlay extends GuiComponent {
-    private final int width;
-    private final int height;
     private final Minecraft mc;
     private ResourceLocation texture = new ResourceLocation(HBDE.MODID, "textures/gui/slot_dumping.png");
 
     public ToggleSlotOverlay() {
         this.mc = Minecraft.getInstance();
-        this.width = this.mc.getWindow().getGuiScaledWidth();
-        this.height = this.mc.getWindow().getGuiScaledHeight();
     }
 
     public void render(int i) {
-        //mc.textureManager.bindForSetup(texture);
+        final int width = this.mc.getWindow().getGuiScaledWidth();
+        final int height = this.mc.getWindow().getGuiScaledHeight();
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 2.0f);
         RenderSystem.setShaderTexture(0, texture);
         blit(new PoseStack(),
                 width / 2 - 90 + i * 20 + 2,
